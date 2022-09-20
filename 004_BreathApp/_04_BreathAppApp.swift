3da5a50e0d666424c0ec80be623082db2215f03d
Nikita Yankov <evne.yankovnikita@gmail.com>
2022-09-20 23:50:55 +0300

    Initial Commit

---
+++ 004_BreathApp/_04_BreathAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  _04_BreathAppApp.swift
+//  004_BreathApp
+//
+//  Created by nikita on 20.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct _04_BreathAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
